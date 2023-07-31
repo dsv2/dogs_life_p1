3e38fd39757dad249da6e722a67418c5fefeee3d
Yaovi Avoudikpon <yaovi.avoud@gmail.com>
2023-07-31 14:43:28 +0100

    commit 5

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,28 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example.superheroes;
 
-import com.example.superheroes.model.Hero;
-import com.example.superheroes.practice.Calculator;
-import com.example.superheroes.repository.HeroRepository;
-import com.example.superheroes.service.HeroService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
-public class SuperheroesApplication{
+public class SuperheroesApplication {
+	public SuperheroesApplication() {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
-
-		public List<Hero> getAllHeroes(){
-			return heroService.getAllHeroes();
-		}
 	}
-
 }
-
-
-
--- superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
+++ superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
@@ -1,29 +1,47 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.example.superheroes.controller;
 
 import com.example.superheroes.model.Hero;
 import com.example.superheroes.service.HeroService;
+import javax.validation.Valid;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@CrossOrigin(
+        origins = {"http://localhost:3000"}
+)
 public class HeroController {
-
-    
-
     @Autowired
     HeroService heroService;
 
-}
-
-public class getHeros {
-
-
-    @GetMapping("/heroes")
-    public List<Hero> getAllHeroes(){
-        return heroService.getAllHeroes();
+    public HeroController() {
     }
-}
+
+    @GetMapping({"/heroes"})
+    public List<Hero> getAllHeroes() {
+        return this.heroService.getAllHeroes();
+    }
+
+    @GetMapping({"/heroes/{letter}"})
+    public List<Hero> getHeroesStartingWithLetter(@PathVariable String letter) {
+        return this.heroService.getHeroesStartingWithLetter(letter);
+    }
+
+   // @PostMapping({"/heroes"})
+    //public HttpStatus saveNewHero(@RequestBody @Valid Hero hero) {
+       // Hero result = this.heroService.saveHero(hero);
+       // return result == null ? HttpStatus.PRECONDITION_FAILED : HttpStatus.CREATED;
+    }
 
