190fa28c9b1acd00bf813ae8dab1fa415d9fe4f9
oforiwaasam <lilyoforiwaas@gmail.com>
2023-08-01 19:34:18 +0100

    all springboot code done

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,25 +1,32 @@
 package com.example.superheroes;
 
+import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
+import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
+public class SuperheroesApplication {
 
 	@Autowired
-	Calculator calc;
+	HeroService heroService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
-		int result = calc.addition(3,4);
-		System.out.println(result);
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
+//		for (Hero hero: heroes) {
+//			System.out.println(hero);
+//		}
+//	}
+
 }
