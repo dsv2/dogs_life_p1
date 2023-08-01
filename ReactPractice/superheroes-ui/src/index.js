55e7c00a506b68f3bcbc32f834251336beb74dee
Zihao Fang <fzhprivate@gmail.com>
2023-08-01 15:12:18 +0100

    Column to be done

--- ReactPractice/superheroes-ui/src/index.js
+++ ReactPractice/superheroes-ui/src/index.js
@@ -4,6 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Batman from "./images/1.jpeg";
+import Superman from "./images/2.jpeg";
+import GreenLantern from "./images/4.jpeg";
+import TheHuntress from "./images/5.jpeg";
+import TheFlash from "./images/3.jpeg";
+import HarleyQuinn from "./images/6.jpeg";
+import Deadshot from "./images/7.jpeg";
+import CountVertigo from "./images/8.jpeg";
+import Robin from "./images/9.jpeg";
+import Nightwing from "./images/10.jpeg";
+
+const images = {
+    Batman,
+    Superman,
+    GreenLantern,
+    TheHuntress,
+    TheFlash,
+    HarleyQuinn,
+    Deadshot,
+    CountVertigo,
+    Robin,
+    Nightwing
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +41,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default images;
